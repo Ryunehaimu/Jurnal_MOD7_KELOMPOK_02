9c35628179fe120af75432f69f648712a9a23cb8
Your Name <you@example.com>
2023-04-05 16:21:16 +0700

    menambahkan class DataMahasiswa1302210025

--- Modul7_Kelompok_02/Modul7_Kelompok_02/Program.cs
+++ Modul7_Kelompok_02/Modul7_Kelompok_02/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_Kelompok_02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa1302210025 dataMahasiswa = new DataMahasiswa1302210025();
+        dataMahasiswa.ReadJSON();
+    }
+}
